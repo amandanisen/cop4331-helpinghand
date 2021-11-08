37b05b7a20eb2d3a4c086abcf4700fae1320867a
Amanda Nisenbaum <amanda.nisenbaum@knights.ucf.edu>
2021-11-08 17:52:32 -0500

    working frontend, no api

--- src/components/login/volunteerLogin.js
+++ src/components/login/volunteerLogin.js
@@ -5,6 +5,15 @@ import Appbar from "../appbar/appbar.js";
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Avatar from '@material-ui/core/Avatar';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+
+
 import { useLocation } from "react-router-dom";
 import axios from 'axios';
 import { connect } from "react-redux";
@@ -16,10 +25,18 @@ const useStyles = makeStyles((theme) => ({
   root: {
     marginTop: 40,
   },
+  image: {
+    marginTop: '5px',
+    height: '100px',
+},
   button: {
     marginTop: '40px',
     width: '110px',
     height: '40px',
+    backgroundColor: "#27AE60",
+    color: '#FFFFFF'
+  },
+  buttonColor: {
     backgroundColor: "#27AE60",
     color: '#FFFFFF'
   },
@@ -46,26 +63,83 @@ function AccessCodePage(props) {
       <>
         <Appbar title={location.state.role} />
         <form onSubmit={handleSubmit}>
-        <Grid container style={{minHeight: '100vh'}}>
-            <Grid item xs={12} sm={6}>
-                <img src="/images/HelpingHands.png" style={{width: '100%', height:'100%', objectFit: 'cover'}} alt="brand"/>
+        <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >   
+        <Grid className={classes.root} item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Grid item>
+                <img className={classes.image} src="/images/Logo.png"></img>
             </Grid>
-            <Grid container item xs={12} sm={6} 
-            alignItems="center" 
-            direction="column"
-            justify="space-between" 
-            style={{padding:10}}>
-                <div />
-                <div style={{display:"flex", flexDirection:"column", maxWidth:300, minWidth:300}}>
-                    <Grid container justify="center">
-                        <img src="/images/Logo.png" alt="logo" style={{width: '50%', height:'100%'}}></img>
-                    </Grid>
-                    <TextField label="Username" margin="normal"/>
-                    <TextField label="Password" margin="normal"/>
-                </div>
-                <div/>
-            </Grid>
+            {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              {/* <LockOutlinedIcon /> */}
+            {/* </Avatar> */}
+            
+            {/* <Typography component="h3" variant="h5">
+              Volunteer Sign In
+            </Typography> */}
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+               className={classes.buttonColor}
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
         </Grid>
+
+          </Grid>
         </form>
       </>
     )

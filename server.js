6bdbb4890ffb312632188c917a071220a9b35998
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2021-11-23 16:41:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,20 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => 
+{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
 // Setup your api routes with express
 app.use("/vol", volRoutes);
 app.use("/coord", coordRoutes);

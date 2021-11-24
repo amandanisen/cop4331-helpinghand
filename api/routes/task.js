3add71f98be88a6c2ca967b4cf359ee3e152c10a
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2021-11-24 13:45:06 -0500

    Update task.js

--- api/routes/task.js
+++ api/routes/task.js
@@ -63,12 +63,12 @@ router.post('/create', async(req, res) =>
 
 });
 
-router.post('/find', async(req, res) =>
+router.get('/find+:email', async(req, res) =>
 {
-  // input: latitude, longitude (of the volunteer searching), range
+  // input: email
   // out: list of tasks
   const db = client.db();
-  const {email} = req.body;
+  const {email} = req.params;
 
   await db.collection('volunteer').findOne({vol_email: email}).then( (user) => {
     if (user == null)

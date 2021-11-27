6feb9c8b06fe6c9b2ba71c172bb44907cd29e422
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2021-11-27 15:30:19 -0500

    Heroku changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,8 +7,14 @@ import HomePage from './components/homepageold/homepageold.js';
 import axios from "axios"
 import { Provider as ReduxProvider } from 'react-redux';
 import store from './redux/store';
-axios.defaults.baseURL = 'http://helpinghand.com';
+axios.defaults.baseURL = 'https://helpinghand-cop4331.herokuapp.com/';
 
+if (process.env.NODE_ENV === "production"){
+  App.use(express.static("build"));
+  App.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
